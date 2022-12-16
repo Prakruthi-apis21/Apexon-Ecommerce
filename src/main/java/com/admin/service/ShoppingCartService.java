3038ff3cc1150915cdee1176f031fa333c79ff49
Prakruthi-apis21 <prakruthi.reddy@apexon.com>
2022-12-15 22:13:57 -0800

    first commit

---
+++ src/main/java/com/admin/service/ShoppingCartService.java
@@ -0,0 +1,20 @@
+package com.admin.service;
+
+import com.admin.exception.NotEnoughProductsInStockException;
+import com.admin.model.Product;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public interface ShoppingCartService {
+
+    void addProduct(Product product);
+
+    void removeProduct(Product product);
+
+    Map<Product, Integer> getProductsInCart();
+
+    void checkout() throws NotEnoughProductsInStockException;
+
+    BigDecimal getTotal();
+}
